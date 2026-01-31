60b6d09dc5060312aae790665e282aa9b309ab53
rebelsumit07 <rebelsumit636@gmail.com>
2026-01-31 21:16:34 +0545

    Update api.js

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an axios instance with your backend URL
 const API = axios.create({
-  baseURL: "https://prototype-xcoa.onrender.com", // your backend server
+  baseURL: "https://prototype-xcoa.onrender.com/api", // your backend server
 });
 
 // Add admin token to all requests if available
@@ -15,3 +15,4 @@ API.interceptors.request.use((config) => {
 });
 
 export default API;
+
